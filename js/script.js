7edc10d1464aef6e503d7108f03fe635b66488ec
sjaworowicz <125sjaworowicz@frhsd.com>
2023-12-07 09:04:19 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+secretMessage .pop();
+
+console.log(secretMessage.length);
+
+secretMessage .push('to' , 'program');
+
+secretMessage .push["easily"] = 'right';
+
+secretMessage .pop();
+
+secretMessage .unshift('program');
+
+secretMessage .pop();
+secretMessage .push["easily"] = 'right';
+secretMessage[7] = 'right';
+
+console.log(secretMessage.join);
